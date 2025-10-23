fb39ccd97ca1b7c0b0d149504f86b0daa50ccce8
Jorge Arias <ja549655@gmail.com>
2025-10-22 21:18:31 -0400

    New Animations

--- src/app/components/ImageCard.tsx
+++ src/app/components/ImageCard.tsx
@@ -1,30 +1,54 @@
 'use client'
 
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import Image from 'next/image'
 
 export default function ImageCard({children, imgSrc, props}:any) {
   const [isOpen, setIsOpen] = useState(false);
+  const [animationStage, setAnimationStage] = useState('closed'); // 'closed', 'opening', 'open', 'closing'
+  const containerRef = useRef<HTMLDivElement>(null);
 
   const handleClick = () => {
-    setIsOpen(!isOpen);
+    if (!isOpen && animationStage === 'closed') {
+      setAnimationStage('opening');
+      setIsOpen(true);
+    } else if (isOpen && animationStage === 'open') {
+      setAnimationStage('closing');
+    }
   };
+
+  // Manejar las transiciones de animación
+  useEffect(() => {
+    if (animationStage === 'opening') {
+      const timer = setTimeout(() => {
+        setAnimationStage('open');
+      }, 600); // Duración de la animación de entrada
+      return () => clearTimeout(timer);
+    } else if (animationStage === 'closing') {
+      const timer = setTimeout(() => {
+        setIsOpen(false);
+        setAnimationStage('closed');
+      }, 500); // Duración de la animación de salida
+      return () => clearTimeout(timer);
+    }
+  }, [animationStage]);
 
   return (
     <div
       {...props}
       className={isOpen ? 'img-wrapper-open' : 'gallery-img'}
       onClick={handleClick}
+      ref={containerRef}
     >
       {isOpen ? (
-        // Vista expandida (modal)
-        <div className='img-wrapper'>
+        // Vista expandida (modal) con animación
+        <div className={`img-wrapper ${animationStage}`}>
           <Image
             src={imgSrc}
             alt=''
-            className='full-img'
+            className={`full-img ${animationStage}`}
           />
-          <div className='full-text'>
+          <div className={`full-text ${animationStage}`}>
             <h2>Type Of Work</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non recusandae facilis fugiat officia neque quas, praesentium ut commodi, laboriosam qui, distinctio assumenda. Consequuntur facere quae tempore assumenda ullam, sint eos!</p>
           </div>
